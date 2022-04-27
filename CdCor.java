2f6906cf734edc0bc9d287f4a7408e083a503911
Rafhael Matias <95319831+RafhaelMatias@users.noreply.github.com>
2022-04-26 23:52:54 -0300

    Add files via upload

---
+++ CdCor.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class CdCor {
+
+    public static void main(String[] args) {
+
+
+        String cor = "";
+
+
+        Scanner resultado = new Scanner(System.in);
+
+        System.out.println("Escolha entre essas cores");
+        System.out.println("verde");
+        System.out.println("azul");
+        System.out.println("amarelo");
+        System.out.println("Vernelho");
+
+
+
+        System.out.println("escolha a cor");
+        cor = resultado.nextLine();
+
+
+        if ( cor.equalsIgnoreCase("verde")){
+            System.out.println("O preço do CD " + cor + " é 10$");
+        };
+        if ( cor.equalsIgnoreCase("azul")){
+            System.out.println("O preço do CD " + cor + " é 20$");
+        };
+        if ( cor.equalsIgnoreCase("amarelo")){
+            System.out.println("O preço do CD " + cor + " é 30$");
+        };
+        if ( cor.equalsIgnoreCase("vermelha")){
+            System.out.println("O preço do CD " + cor + " é 40$");
+        };
+
+        resultado.close();
+
+
+
+
+    }
+}
